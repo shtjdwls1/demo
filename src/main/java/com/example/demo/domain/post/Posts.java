b8c94d09c76c5eab4de476eef7a885f402f2bb6e
노성진 <nosj703@naver.com>
2022-10-05 17:29:45 +0900

    10.5 2차

---
+++ src/main/java/com/example/demo/domain/post/Posts.java
@@ -0,0 +1,36 @@
+package com.example.demo.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity // 테이블과 링크될 클래스임을 나타낸다
+// 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭합니다.
+// ex) SalesManager.java -> sales_manager table
+public class Posts {
+
+    @Id // 해당 테이블의 PK 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성규칙을 나타냄
+    // 스프링부트 2.0에서는 GenerationType.IDENTITY 옵션을 추가해야만 auto_increment가 됨
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당클래스의 필드는 모두 칼럼이 된다
+    // 사용하는 이유는, 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용한다
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성 . 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
