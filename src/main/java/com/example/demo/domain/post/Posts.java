3ddb99ab63018575e4ca77e8ed1850f51a266063
노성진 <nosj703@naver.com>
2022-10-06 18:10:14 +0900

    10.6 JPA마무리

--- src/main/java/com/example/demo/domain/post/Posts.java
+++ src/main/java/com/example/demo/domain/post/Posts.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.post;
 
+import com.example.demo.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Entity // 테이블과 링크될 클래스임을 나타낸다
 // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭합니다.
 // ex) SalesManager.java -> sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성규칙을 나타냄
@@ -32,5 +33,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title=title;
+        this.content=content;
     }
 }
